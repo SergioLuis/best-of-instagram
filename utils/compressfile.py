f8446126eba371a7c8f4c0f17334c4dc9476008a
Sergio Luis Para <sergio@sergioluis.es>
2017-12-16 16:40:52 +0000

    Updated the compressfile.py script to customize how many characters per line are printed. This way, it is way easier copying the compressed result and pasting it to another python file.

--- utils/compressfile.py
+++ utils/compressfile.py
@@ -7,16 +7,30 @@ import base64
 import sys
 import zlib
 
+def print_string(string, splitting, chars_per_line):
+    """If 'splitting', prints 'chars_per_line' characters per line."""
+    if not splitting:
+        print(string)
+        return
+
+    i = 0
+    while i < len(string):
+        print(string[i:i+chars_per_line])
+        i = i + chars_per_line
+
+
 if __name__ == '__main__':
-    if len(sys.argv) is not 2:
-        print("Usage: python compressfile.py [input file]")
+    if len(sys.argv) < 2:
+        print("Usage: python compressfile.py [input file] {chars per line}")
         sys.exit(1)
 
     with open(sys.argv[1], 'r') as input_file:
-        print(
+        print_string(
             base64.b64encode(
                 zlib.compress(
                     bytes(input_file.read(), input_file.encoding)
                 )
-            )
+            ),
+            len(sys.argv) is 3,
+            int(sys.argv[2]) if len(sys.argv) is 3 else 65
         )
