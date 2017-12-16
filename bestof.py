d8af83f31faa94f16aa3e3bc8b166facd46bcd2f
Sergio Luis Para <sergio@sergioluis.es>
2017-12-16 15:52:19 +0000

    Huge code refactor. Now it is way more readable!

--- bestof.py
+++ bestof.py
@@ -3,13 +3,14 @@ shaddy websites!
 """
 
 import base64
+import ctypes
 import json
 import logging
+import os
 import time
 import webbrowser
 import zlib
 
-from os import path
 from threading import Thread
 from flask import Flask, Response, request
 from werkzeug.serving import make_server
@@ -17,26 +18,30 @@ from werkzeug.serving import make_server
 class Instagram(object):
     """Lightweight wrapper for the Instagram API."""
 
-    def authenticate(self):
-        """Asks the user to gran the app permissions, and grabs the access token."""
-        if Instagram.__load_access_token() is not None:
-            return
+    # Compressed just to avoid bots scanning for such information.
+    _b64_client_id = b'eJwzNbMwT0lOTbYwskwyMTAwtkgyM000MUkyT0xKtkyyNAIAjuEIyQ=='
+    _auth = '.instagram_access_token'
 
-        client_id = Instagram.__load_client_id()
-        if client_id is None:
-            raise Exception(
-                """You need to save the ClientID in {} as a JSON file as: """
-                """     '{{ "client_id" : "the_client_id" }}' """.format(
-                    Instagram.__get_file_path(Instagram.__settings)
-                )
-            )
+    _auth_url_format = 'https://api.instagram.com/oauth/authorize/?client_id={}&redirect_uri={}&response_type=token'
+    _redirect_uri = 'http://localhost:5000'
+
+    _internal_token_send_html = b'eJyVVlGP2zYMfs+vUFUMSYBzfEGBokjjDLf0DiiwrX24ARuKYlAk2hZOljyJThoU999HOckl9rkbTg+JKJEf+VEkk+WrD5/W9399vmUlVmY1Wp6+QKjViNFaVoCCyVL4AJjxBvPkHWfp5WWJWCfwT6O3Gf8z+eMmWbuqFqg3BjiTziJYsvx4m4EqgB8tUaOB1S8QkLmcYQlsD8Iv08P5BboVFWR8q2FXO48XgDutsMwUbLWEpBWumLYatTBJkMJANj85M9o+MA8m4wH3BkIJQEilhzzjMfqwSNOccMOscK4wIGodZtJVqQzh51xU2uyz3+I9eC/whNpiHfZxbZzas+9PYnsk5EPhXWNVIp1xfsFe3919mN/N33fUouvk4GbBxmdH4ysWhA0JSTo/mzyOnrZKb2e5pwzd9zy3+Viw+fX1T11ftVBK22LBrrvnSofaCPKPgt6te1eCLkocRHOBEu7sgolNcKbBniW6+pknAzk+O6yEL7TtHA/SXPd4/ojOFjxqKoJEGF0QbqWV6tPqUk4kGPPSoI7FOJz8t9fX9bcfIDLRoOvlCr7hKVxJoOAv3cbPZXosuWV6aNBlW3POGidUxqWosfFw7x7ATqZUpccylV7XeK7TvLEyvhnr6vc4bIVnQkoI4W+MCiwjWla53cw4KaL9LPbPjFKoccJf8+mX+deTlLXS+1G/VnAyTj2ExmBKtf2dtciLrp/HKzYuAMfToZw/xd6C0ZChnq8FVUa4YjQtSqeGeOTOVxS/crKpKLEz6UEg3BqI0oTHaz7tt6SvZjTxbhC93lBdT/gBnz85+l8D0YbKY4RYTnvZIP1JjO0B9jS1jiT6wcel88nhclaK8GlnP3tXU3HvJ2Q5HTI4sS6p5MHeaTDqP8hrWzfYZ39aFxA9brivgZjxg8bL7eNUJ/vI4aWmW2Ea4Kd3/0IQX/vJ7TyKqGuwal1qoyYXqAN+H0ddqTstTgmMPdcBjV4Gq6HZVNQN04E2PrbkQaRBwqQRIWT8MOT4uVmfX64vbvsax3HU02i1yvnqpm0zVnhBWuoVDZH5kOKb1e9ux/auoQFh6VeTZoSlTo0nnq1//Uh2b3ohpBTDRcxn8bhdpjFp7dyK/y/+BR9DTnw='
+    _success_html = b'eJyVVF1vmzAUfe+vuGWa+lJCUKVpYpCpSxup0r4eOml7dOwLWDU2sy9Jo2n/fSaQBkj6MD+AfT/OOfdyTXp59235+Ov7PZRUqcVFenghE4sL8CutkBjwklmHlAUN5eH7AKKhsySqQ/zdyE0W/Ax/3IZLU9WM5FphANxoQu0zH+4zFAUGfSZJUrj4hI7A5EAlwg6ZTaPOPkDXrMIs2Ejc1sbSAHArBZWZwI3kGO4P1yC1JMlU6DhTmMUHMiX1E1hUWeBop9CViB6ptJhnQaveJVGUe1w3K4wpFLJauhk3VcSd+5izSqpd9qX1o7WMDqh7rG7frrURO/jzctybGH8qrGm0CLlRxibwZrW6i1fxh1FYSx12NAlcHYmursEx7UJ/kvkx5e/Fy1bIzSy3vkOPE+Z9PxKI5/O3Y66aCSF1kcB8bBfS1Yp5fmL+u419JcqipLNoxvmGG50AWzujGppkkqlPmBTmdGKsmC2kHpnPlrmc1PlaORu0JP0QhEzJwuNWUohpWeOSQ45K/a+ofhjPN//dfF4/v4IIrCEz6RU+00Eu96Boh7TtM436kUuj7oKm7cz10+jlAFfMuSzoWhUcR/PUuRx4pxF9UZOIfVQZL245R+egsMxHiUsvJT4XeLP4arawMw1wpv3do8ZqPw6txcLy84PPu5lIiLyGgebjsd+mUVeuT23/Uv8A/z1DHw=='
+
+
+    def authenticate(self):
+        """If necessary, asks the user to grant the app permissions, and saves
+        the access token to the user's home directory.
+        """
+        if self._access_token is not None:
+            return
 
         auth_server = FlaskAppWrapper()
         auth_server.add_endpoint(
             endpoint="/",
             endpoint_name="authorization_redirect",
             response=Response(
-                Instagram.__decompress_html(self.__internal_token_send_html)
+                _Utils.decompress_text(self._internal_token_send_html)
             )
         )
         auth_server.add_endpoint(
@@ -44,11 +49,11 @@ class Instagram(object):
             endpoint_name="authorization_success",
             handler=self._handle_instagram_token,
             response=Response(
-                Instagram.__decompress_html(self.__success_html)
+                _Utils.decompress_text(self._success_html)
             )
         )
         auth_server.run()
-        webbrowser.open(Instagram.__build_oauth_url(client_id))
+        webbrowser.open(self._auth_url)
 
         while not self._is_authorized():
             time.sleep(1)
@@ -57,81 +62,83 @@ class Instagram(object):
 
 
     def _handle_instagram_token(self, args):
-        Instagram.__save_access_token(args['token'])
+        self._access_token = args['token']
 
 
     def _is_authorized(self):
-        return path.isfile(Instagram.__get_file_path(Instagram.__auth))
+        return os.path.isfile(_Utils.get_home_based_path(Instagram._auth))
 
 
-    @staticmethod
-    def __build_oauth_url(client_id):
-        return Instagram.__auth_url.format(
-            client_id,
-            Instagram.__redirect_uri
+    @property
+    def _auth_url(self):
+        return self._auth_url_format.format(
+            self._client_id,
+            self._redirect_uri
         )
 
 
-    @staticmethod
-    def __save_access_token(access_token):
-        setting = {'access_token': access_token}
-        with open(Instagram.__get_file_path(Instagram.__auth), 'w') as config:
-            json.dump(setting, config)
+    @property
+    def _access_token(self):
+        access_token_file = _Utils.get_home_based_path(self._auth)
 
-
-    @staticmethod
-    def __load_access_token():
-        config_file = Instagram.__get_file_path(Instagram.__auth)
-        if not path.isfile(config_file):
+        if not os.path.isfile(access_token_file):
             return None
 
-        with open(config_file) as config:
-            return json.load(config).get('access_token', None)
+        with open(access_token_file) as config:
+            return config.read()
+
+
+    @_access_token.setter
+    def _access_token(self, value):
+        access_token_file = _Utils.get_home_based_path(self._auth)
+
+        with open(access_token_file, 'w') as config:
+            config.write(value)
+
+        if os.name == 'nt':
+            _Utils.set_hidden_flag(access_token_file)
+
+
+    @property
+    def _client_id(self):
+        return _Utils.decompress_text(self._b64_client_id)
+
+
+class _Utils(object):
+    """Utility methods not extrictly related to anything else."""
+
+    @staticmethod
+    def decompress_text(text):
+        """Takes a text compressed with ZLib and encoded in Base64 and returns
+        the original text.
+        """
+        return zlib.decompress(base64.b64decode(text)).decode('utf-8')
 
 
     @staticmethod
-    def __load_client_id():
-        config_file = Instagram.__get_file_path(Instagram.__settings)
-        if not path.isfile(config_file):
-            return None
-
-        with open(config_file) as config:
-            return json.load(config).get('client_id', None)
+    def get_home_based_path(file_name):
+        """Returns the 'file_name' path preprended with the home path of the
+        user. Works for all OSs.
+        """
+        return os.path.join(os.path.expanduser('~'), file_name)
 
 
     @staticmethod
-    def __get_file_path(file_name):
-        return path.join(path.expanduser('~'), file_name)
-
-
-    @staticmethod
-    def __decompress_html(html):
-        return zlib.decompress(base64.b64decode(html)).decode('utf-8')
-
-
-    __settings = 'instagram_settings.json'
-    __auth = 'instagram_auth.json'
-
-    __auth_url = 'https://api.instagram.com/oauth/authorize/?client_id={}&redirect_uri={}&response_type=token'
-    __redirect_uri = 'http://localhost:5000'
-
-    __internal_token_send_html = b'eJyVVlGP2zYMfs+vUFUMSYBzfEGBokjjDLf0DiiwrX24ARuKYlAk2hZOljyJThoU999HOckl9rkbTg+JKJEf+VEkk+WrD5/W9399vmUlVmY1Wp6+QKjViNFaVoCCyVL4AJjxBvPkHWfp5WWJWCfwT6O3Gf8z+eMmWbuqFqg3BjiTziJYsvx4m4EqgB8tUaOB1S8QkLmcYQlsD8Iv08P5BboVFWR8q2FXO48XgDutsMwUbLWEpBWumLYatTBJkMJANj85M9o+MA8m4wH3BkIJQEilhzzjMfqwSNOccMOscK4wIGodZtJVqQzh51xU2uyz3+I9eC/whNpiHfZxbZzas+9PYnsk5EPhXWNVIp1xfsFe3919mN/N33fUouvk4GbBxmdH4ysWhA0JSTo/mzyOnrZKb2e5pwzd9zy3+Viw+fX1T11ftVBK22LBrrvnSofaCPKPgt6te1eCLkocRHOBEu7sgolNcKbBniW6+pknAzk+O6yEL7TtHA/SXPd4/ojOFjxqKoJEGF0QbqWV6tPqUk4kGPPSoI7FOJz8t9fX9bcfIDLRoOvlCr7hKVxJoOAv3cbPZXosuWV6aNBlW3POGidUxqWosfFw7x7ATqZUpccylV7XeK7TvLEyvhnr6vc4bIVnQkoI4W+MCiwjWla53cw4KaL9LPbPjFKoccJf8+mX+deTlLXS+1G/VnAyTj2ExmBKtf2dtciLrp/HKzYuAMfToZw/xd6C0ZChnq8FVUa4YjQtSqeGeOTOVxS/crKpKLEz6UEg3BqI0oTHaz7tt6SvZjTxbhC93lBdT/gBnz85+l8D0YbKY4RYTnvZIP1JjO0B9jS1jiT6wcel88nhclaK8GlnP3tXU3HvJ2Q5HTI4sS6p5MHeaTDqP8hrWzfYZ39aFxA9brivgZjxg8bL7eNUJ/vI4aWmW2Ea4Kd3/0IQX/vJ7TyKqGuwal1qoyYXqAN+H0ddqTstTgmMPdcBjV4Gq6HZVNQN04E2PrbkQaRBwqQRIWT8MOT4uVmfX64vbvsax3HU02i1yvnqpm0zVnhBWuoVDZH5kOKb1e9ux/auoQFh6VeTZoSlTo0nnq1//Uh2b3ohpBTDRcxn8bhdpjFp7dyK/y/+BR9DTnw='
-    __success_html = b'eJyVVF1vmzAUfe+vuGWa+lJCUKVpYpCpSxup0r4eOml7dOwLWDU2sy9Jo2n/fSaQBkj6MD+AfT/OOfdyTXp59235+Ov7PZRUqcVFenghE4sL8CutkBjwklmHlAUN5eH7AKKhsySqQ/zdyE0W/Ax/3IZLU9WM5FphANxoQu0zH+4zFAUGfSZJUrj4hI7A5EAlwg6ZTaPOPkDXrMIs2Ejc1sbSAHArBZWZwI3kGO4P1yC1JMlU6DhTmMUHMiX1E1hUWeBop9CViB6ptJhnQaveJVGUe1w3K4wpFLJauhk3VcSd+5izSqpd9qX1o7WMDqh7rG7frrURO/jzctybGH8qrGm0CLlRxibwZrW6i1fxh1FYSx12NAlcHYmursEx7UJ/kvkx5e/Fy1bIzSy3vkOPE+Z9PxKI5/O3Y66aCSF1kcB8bBfS1Yp5fmL+u419JcqipLNoxvmGG50AWzujGppkkqlPmBTmdGKsmC2kHpnPlrmc1PlaORu0JP0QhEzJwuNWUohpWeOSQ45K/a+ofhjPN//dfF4/v4IIrCEz6RU+00Eu96Boh7TtM436kUuj7oKm7cz10+jlAFfMuSzoWhUcR/PUuRx4pxF9UZOIfVQZL245R+egsMxHiUsvJT4XeLP4arawMw1wpv3do8ZqPw6txcLy84PPu5lIiLyGgebjsd+mUVeuT23/Uv8A/z1DHw=='
-
-    __login_endpoint = "/"
+    def set_hidden_flag(file_path):
+        """Sets the 'hidden file' flag to the specified file."""
+        ctypes.windll.kernel32.SetFileAttributesW(file_path, 0x02)
 
 
 class FlaskAppWrapper(object):
     """Flask server to handle Instagram authentication process."""
 
     app = None
-    instagram = None
     server = None
 
     def __init__(self):
         flask_logger = logging.getLogger("werkzeug")
         flask_logger.setLevel(logging.ERROR)
-        self.app = Flask(FlaskAppWrapper.__name)
+        self.app = Flask('FlaskAuthenticationServer')
 
 
     def run(self):
@@ -152,8 +159,6 @@ class FlaskAppWrapper(object):
             endpoint_name,
             _EndpointAction(handler, response).execute
         )
-
-    __name = 'FlaskAuthenticationServer'
 
 
 class _ServerThread(Thread):
@@ -176,7 +181,7 @@ class _ServerThread(Thread):
         self.srv.shutdown()
 
 
-class _EndpointAction(object):
+class _EndpointAction(object): # pyling: disable=R0903
     """Represents an action to be executed when an endpoint is hit."""
 
     def __init__(self, action, response):
@@ -193,5 +198,5 @@ class _EndpointAction(object):
 
 
 if __name__ == '__main__':
-    instagram = Instagram()
-    instagram.authenticate()
+    testing = Instagram()
+    testing.authenticate()
