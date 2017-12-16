6e934636833b05a69d1c5f5987a38e31e5c87293
Sergio Luis Para <sergio@sergioluis.es>
2017-12-16 16:49:31 +0000

    Updated the hardcoded blob'd HTMLs in the main code.

--- bestof.py
+++ bestof.py
@@ -22,11 +22,46 @@ class Instagram(object):
     _b64_client_id = b'eJwzNbMwT0lOTbYwskwyMTAwtkgyM000MUkyT0xKtkyyNAIAjuEIyQ=='
     _auth = '.instagram_access_token'
 
-    _auth_url_format = 'https://api.instagram.com/oauth/authorize/?client_id={}&redirect_uri={}&response_type=token'
+    _auth_url_format = (
+        """https://api.instagram.com/oauth/authorize/"""
+        """?client_id={}&redirect_uri={}&response_type=token"""
+    )
+
     _redirect_uri = 'http://localhost:5000'
 
-    _internal_token_send_html = b'eJyVVlGP2zYMfs+vUFUMSYBzfEGBokjjDLf0DiiwrX24ARuKYlAk2hZOljyJThoU999HOckl9rkbTg+JKJEf+VEkk+WrD5/W9399vmUlVmY1Wp6+QKjViNFaVoCCyVL4AJjxBvPkHWfp5WWJWCfwT6O3Gf8z+eMmWbuqFqg3BjiTziJYsvx4m4EqgB8tUaOB1S8QkLmcYQlsD8Iv08P5BboVFWR8q2FXO48XgDutsMwUbLWEpBWumLYatTBJkMJANj85M9o+MA8m4wH3BkIJQEilhzzjMfqwSNOccMOscK4wIGodZtJVqQzh51xU2uyz3+I9eC/whNpiHfZxbZzas+9PYnsk5EPhXWNVIp1xfsFe3919mN/N33fUouvk4GbBxmdH4ysWhA0JSTo/mzyOnrZKb2e5pwzd9zy3+Viw+fX1T11ftVBK22LBrrvnSofaCPKPgt6te1eCLkocRHOBEu7sgolNcKbBniW6+pknAzk+O6yEL7TtHA/SXPd4/ojOFjxqKoJEGF0QbqWV6tPqUk4kGPPSoI7FOJz8t9fX9bcfIDLRoOvlCr7hKVxJoOAv3cbPZXosuWV6aNBlW3POGidUxqWosfFw7x7ATqZUpccylV7XeK7TvLEyvhnr6vc4bIVnQkoI4W+MCiwjWla53cw4KaL9LPbPjFKoccJf8+mX+deTlLXS+1G/VnAyTj2ExmBKtf2dtciLrp/HKzYuAMfToZw/xd6C0ZChnq8FVUa4YjQtSqeGeOTOVxS/crKpKLEz6UEg3BqI0oTHaz7tt6SvZjTxbhC93lBdT/gBnz85+l8D0YbKY4RYTnvZIP1JjO0B9jS1jiT6wcel88nhclaK8GlnP3tXU3HvJ2Q5HTI4sS6p5MHeaTDqP8hrWzfYZ39aFxA9brivgZjxg8bL7eNUJ/vI4aWmW2Ea4Kd3/0IQX/vJ7TyKqGuwal1qoyYXqAN+H0ddqTstTgmMPdcBjV4Gq6HZVNQN04E2PrbkQaRBwqQRIWT8MOT4uVmfX64vbvsax3HU02i1yvnqpm0zVnhBWuoVDZH5kOKb1e9ux/auoQFh6VeTZoSlTo0nnq1//Uh2b3ohpBTDRcxn8bhdpjFp7dyK/y/+BR9DTnw='
-    _success_html = b'eJyVVF1vmzAUfe+vuGWa+lJCUKVpYpCpSxup0r4eOml7dOwLWDU2sy9Jo2n/fSaQBkj6MD+AfT/OOfdyTXp59235+Ov7PZRUqcVFenghE4sL8CutkBjwklmHlAUN5eH7AKKhsySqQ/zdyE0W/Ax/3IZLU9WM5FphANxoQu0zH+4zFAUGfSZJUrj4hI7A5EAlwg6ZTaPOPkDXrMIs2Ejc1sbSAHArBZWZwI3kGO4P1yC1JMlU6DhTmMUHMiX1E1hUWeBop9CViB6ptJhnQaveJVGUe1w3K4wpFLJauhk3VcSd+5izSqpd9qX1o7WMDqh7rG7frrURO/jzctybGH8qrGm0CLlRxibwZrW6i1fxh1FYSx12NAlcHYmursEx7UJ/kvkx5e/Fy1bIzSy3vkOPE+Z9PxKI5/O3Y66aCSF1kcB8bBfS1Yp5fmL+u419JcqipLNoxvmGG50AWzujGppkkqlPmBTmdGKsmC2kHpnPlrmc1PlaORu0JP0QhEzJwuNWUohpWeOSQ45K/a+ofhjPN//dfF4/v4IIrCEz6RU+00Eu96Boh7TtM436kUuj7oKm7cz10+jlAFfMuSzoWhUcR/PUuRx4pxF9UZOIfVQZL245R+egsMxHiUsvJT4XeLP4arawMw1wpv3do8ZqPw6txcLy84PPu5lIiLyGgebjsd+mUVeuT23/Uv8A/z1DHw=='
+    _auth_first_stage = (
+        b'eJyVVlGL2zgQft9foVMpcWBtJxTKkYtT2nQX7uHoUvbgjlIORRrHYmXLlcZJQ9n/fiM'
+        b'72cRe947VQ+KRZr6ZbzQz9vKXj5/W93/f3bACS7O6Wp7+QKjVFaO1LAEFk4VwHjDjDe'
+        b'bxr5yll4cFYh3Dt0bvMv5X/Of7eG3LWqDeGOBM2gqhIsvfbzJQW+BHS9RoYPUBPDKbM'
+        b'yyAHUC4ZdrtX6BXooSM7zTsa+vwAnCvFRaZgp2WELfCNdOVRi1M7KUwkM1PzoyuHpgD'
+        b'k3GPBwO+ACCkwkGe8RC9X6RpTrg+2Vq7NSBq7RNpy1R6/y4XpTaH7I9wDs4JPKG2WN1'
+        b'zWBurDuzHk9huCfmwdbapVCytsW7BXt3efpzfzn/rqQXXcedmwSZnR5Nr5kXlY5J0fj'
+        b'Z5vHp6VHqX5I4ydD/w3OZjweaz2eu+r1oopavtgs36+0r72gjyj4LurX9WgN4WOIpmP'
+        b'SXcVgsmNt6aBgeWaOtnngzk+GyzFG6rq972KM31gOfP6OzAoaYiiIXRW8IttVJDWn3K'
+        b'sQRjXhrUsRjHk/92Nqu//wSRiQbtIFfwHU/hSgIFd+k2/C7TY8kt065Bl23N2cpYoTI'
+        b'uRY2Ng3v7AFU0pSo9lql0usZzneZNJcOdsb7+gMNOOCakBO//waDAMqJVKbtPjJUi2C'
+        b'ehfxJKocaIv+LTL/OvJylrpWelgtEkdeAbgymV9g/WAi/6bh6v2WQLOJmOpfwp9BaMZ'
+        b'gy1fC2oMPw1o2FRWDVGI7eupPCVlU1JeU2kA4FwYyBIEQ/HfDrsSFcmNPDeIzq9obKO'
+        b'eIfPnxz9r4FoQ+UhQixIfagfhdge4EBD60hiGHxYOo+6w6QQ/tO+unO2pto+RGQ5HTM'
+        b'4sS6o4qG61WDUf5DXVd3gkP1pXUAMuOGhBmLGO42X24ehTvaBw0tNd8I0wE/3/oUgvg'
+        b'6T27sUUddQqXWhjYouUEf8Pl71pf6wOCUwtFwPNHgZrYZmU1IzTEe6+NiRnUhzhEkjv'
+        b'M94N+P4uVefH64vTocax2k00Gi1ivnqM3iKmYZl+7btug1tKxAdo7uuptEyH7N/s7qj'
+        b'96IHthcakyQhvTeDSFIK5SL0s3h8XKYhd+30Cl8Z/wLciVFf'
+    )
+
+    _auth_second_stage = (
+        b'eJyVVF1vmzAUfe+vuGWa+lJCUKVpYpCpSxup0r4eOml7dOwLWDU2sy9Jo2n/fSaQBkj'
+        b'6MD+AfT/OOfdyTXp59235+Ov7PZRUqcVFenghE4sL8CutkBjwklmHlAUN5eH7AKKhsy'
+        b'SqQ/zdyE0W/Ax/3IZLU9WM5FphANxoQu0zH+4zFAUGfSZJUrj4hI7A5EAlwg6ZTaPOP'
+        b'kDXrMIs2Ejc1sbSAHArBZWZwI3kGO4P1yC1JMlU6DhTmMUHMiX1E1hUWeBop9CViB6p'
+        b'tJhnQaveJVGUe1w3K4wpFLJauhk3VcSd+5izSqpd9qX1o7WMDqh7rG7frrURO/jzcty'
+        b'bGH8qrGm0CLlRxibwZrW6i1fxh1FYSx12NAlcHYmursEx7UJ/kvkx5e/Fy1bIzSy3vk'
+        b'OPE+Z9PxKI5/O3Y66aCSF1kcB8bBfS1Yp5fmL+u419JcqipLNoxvmGG50AWzujGppkk'
+        b'qlPmBTmdGKsmC2kHpnPlrmc1PlaORu0JP0QhEzJwuNWUohpWeOSQ45K/a+ofhjPN//d'
+        b'fF4/v4IIrCEz6RU+00Eu96Boh7TtM436kUuj7oKm7cz10+jlAFfMuSzoWhUcR/PUuRx'
+        b'4pxF9UZOIfVQZL245R+egsMxHiUsvJT4XeLP4arawMw1wpv3do8ZqPw6txcLy84PPu5'
+        b'lIiLyGgebjsd+mUVeuT23/Uv8A/z1DHw=='
+    )
 
 
     def authenticate(self):
@@ -36,12 +71,12 @@ class Instagram(object):
         if self._access_token is not None:
             return
 
-        auth_server = FlaskAppWrapper()
+        auth_server = _FlaskAppWrapper()
         auth_server.add_endpoint(
             endpoint="/",
             endpoint_name="authorization_redirect",
             response=Response(
-                _Utils.decompress_text(self._internal_token_send_html)
+                _Utils.decompress_text(self._auth_first_stage)
             )
         )
         auth_server.add_endpoint(
@@ -49,7 +84,7 @@ class Instagram(object):
             endpoint_name="authorization_success",
             handler=self._handle_instagram_token,
             response=Response(
-                _Utils.decompress_text(self._success_html)
+                _Utils.decompress_text(self._auth_second_stage)
             )
         )
         auth_server.run()
@@ -129,7 +164,7 @@ class _Utils(object):
         ctypes.windll.kernel32.SetFileAttributesW(file_path, 0x02)
 
 
-class FlaskAppWrapper(object):
+class _FlaskAppWrapper(object):
     """Flask server to handle Instagram authentication process."""
 
     app = None
